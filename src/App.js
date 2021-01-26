9dae2cc7c294529fbc894f6a5fcda5ce2824386b
Regis Tesson <registesson@gmail.com>
2021-01-26 13:24:08 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // Global Style
-import AboutUs from './pages/AboutUs'
-// Import Pages
 import GlobalStyle from './components/GlobalStyle'
+// Import Pages
+import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
+import Nav from './components/Nav'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
